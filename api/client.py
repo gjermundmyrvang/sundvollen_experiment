0178ef19b8b4e5d8c83decc500383317dd49d322
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-08-26 17:40:03 +0200

    chore: set up AI client

---
+++ api/client.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+from openai import OpenAI
+from ecologits import EcoLogits
+
+# Initialize EcoLogits
+EcoLogits.init(providers=["openai"])
+
+client = OpenAI(api_key=st.secrets["OPEN_AI_TEST_KEY"])
+
+CONDITION = st.secrets["CONDITION"]
+MODEL = "gpt-4o-mini"
